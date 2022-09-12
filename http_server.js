2ba4d8cb68d56f36f835f68bb559fcb45eb7caf2
kai5363 <57265307+kai5363@users.noreply.github.com>
2022-09-12 12:58:46 +0800

    feat: create a http server to response master ip

---
+++ http_server.js
@@ -0,0 +1,28 @@
+require('dotenv').config();
+const http = require('http');
+const url = require('url');
+const { redis } = require('./redis');
+
+const { MASTER_KEY } = process.env;
+const HTTPPort = 3006;
+
+async function getMaster() {
+  const master = await redis.get(MASTER_KEY);
+  return master;
+}
+
+const server = http.createServer(async (req, res) => {
+  const reqUrl = url.parse(req.url).pathname;
+  if (req.method === 'GET') {
+    if (reqUrl === '/') {
+      const master = await getMaster();
+      res.writeHead(200);
+      res.write(master);
+      res.end();
+    }
+  }
+});
+
+server.listen(HTTPPort, () => {
+  console.log(`HTTP server is listen on prot ${HTTPPort}....`);
+});
